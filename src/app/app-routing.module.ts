5cbfd8d2360ce9305ed6a4d0dc1c284626ec5ac9
tarun20r11a6217 <20r11a6217@gcet.edu.in>
2022-08-30 11:31:11 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouteGaurdService } from './service/route-gaurd.service';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  { path:'', component: LoginComponent}, 
+  { path:'welcome/:name', component: WelcomeComponent},
+  { path:'login', component: LoginComponent},
+  { path:'welcome', component: WelcomeComponent},
+  { path:'todos', component: ListTodosComponent},
+  { path:'logout', component:LogoutComponent},
+  { path:'welcome/:name', component: WelcomeComponent, canActivate:[RouteGaurdService]},
+  { path:'todos', component: ListTodosComponent, canActivate:[RouteGaurdService]},
+  { path:'logout', component:LogoutComponent, canActivate:[RouteGaurdService]},
+  { path:'**', component: ErrorComponent}
+
+  
+  ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
